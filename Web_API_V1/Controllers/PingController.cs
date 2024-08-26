9d26579bda42838ee5ef81d544dce0b768c63f59
David Clayton <david@davidclayton.me>
2024-08-25 20:17:31 -0700

    Created project and Ping API

---
+++ Web_API_V1/Controllers/PingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_API_V1.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Ping")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("Pong");
+        }
+    }
+}
