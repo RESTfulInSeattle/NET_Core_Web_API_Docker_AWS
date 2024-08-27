841549b2edf2c5bc84e5a979293db8212c23a9e5
David Clayton <david@davidclayton.me>
2024-08-27 16:00:39 -0700

    Experimenting with custom middleware

---
+++ Web_API_V1/Middleware/TimingMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Server.HttpSys;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Web_API_V1.Middleware
+{
+    public class TimingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<TimingMiddleware> _logger;
+
+        public TimingMiddleware(ILogger<TimingMiddleware> logger, 
+            RequestDelegate next)
+        {
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext ctx)
+        {
+            var start = DateTime.UtcNow;
+            //Pass the context
+            await _next.Invoke(ctx);
+            _logger.LogInformation($"Timing {ctx.Request.Path}: {(DateTime.UtcNow - start).TotalMilliseconds} ms");
+
+        }
+    }
+
+    public static class TimingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseTimingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<TimingMiddleware>();
+        }
+
+        /*
+        public static void AddTiming(this IServiceCollection svcs)
+        {
+
+           svcs.AddTransient<ITiming, SomeTiming>();
+        }
+        */
+    }
+}
--- Web_API_V1/Program.cs
+++ Web_API_V1/Program.cs
@@ -1,3 +1,5 @@
+using Web_API_V1.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +17,27 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+/*Experimenting with basic Middleware
+app.Use(async (ctx, next) =>
+{
+    var start = DateTime.UtcNow;
+    //Pass the context
+    await next.Invoke(ctx);
+    app.Logger.LogInformation($"Request {ctx.Request.Path}: {(DateTime.UtcNow - start).TotalMilliseconds} ms");
+});
+
+
+app.Use((HttpContext ctx, Func<Task> next) =>
+{
+    app.Logger.LogInformation("Terminating the Request");
+    return Task.CompletedTask;
+});
+
+app.UseMiddleware<TimingMiddleware>();
+*/
+
+app.UseTimingMiddleware();
 
 app.UseHttpsRedirection();
 
